483e33929ce524716f10a8f92071ef475d8ce6a7
Tala Abdallah <talaabdallah@Talas-MacBook-Air.local>
2026-07-05 16:16:30 +0300

    Install Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2EVZG6D1ZH"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2EVZG6D1ZH');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
